bbe7179c4a2334653bf2703f7965f340548ad19e
hunter-henderson <hhenderson10@cub.uca.edu>
2024-03-31 16:39:58 -0500

    Implemented Question 2 Draft Code

--- Project3.py
+++ Project3.py
@@ -10,6 +10,10 @@ import matplotlib.pyplot as plt
 import collections
 from collections import defaultdict
 from heapq import *
+import time
+import heapq
+#import resource
+from itertools import groupby
 
 
 # Class for implementing Dijskra's algorithm
@@ -26,6 +30,20 @@ class Graph:
     self.edges[from_node].append(to_node)
     self.edges[to_node].append(from_node)
     self.distances[(from_node, to_node)] = distance
+    
+# Tracker to help keep track of various components
+class Tracker(object):
+    """Keeps track of the current time, current source, component leader,
+    finish time of each node and the explored nodes.
+    
+    'self.leader' is informs of {node: leader, ...}."""
+
+    def __init__(self):
+        self.current_time = 0
+        self.current_source = None
+        self.leader = {}
+        self.finish_time = {}
+        self.explored = set()
 
 
 # Draws graph and prints result to Console
@@ -49,6 +67,31 @@ def dfs(graph, start):
             visited.add(vertex)
             stack.extend(graph[vertex] - visited)
     return visited
+
+
+# Depth First Search variant with Tracker; for Question 2
+def dfs_tracker(graph_dict, node, tracker):
+    """Inner loop explores all nodes in a SCC. Graph represented as a dict,
+    {tail: [head_list], ...}. Depth first search runs recursively and keeps
+    track of the parameters"""
+
+    tracker.explored.add(node)
+    tracker.leader[node] = tracker.current_source
+    for head in graph_dict[node]:
+        if head not in tracker.explored:
+            dfs_tracker(graph_dict, head, tracker)
+    tracker.current_time += 1
+    tracker.finish_time[node] = tracker.current_time
+
+# Depth First Search Loop; for Question 2
+def dfs_loop(graph_dict, nodes, tracker):
+    """Outer loop checks out all SCCs. Current source node changes when one
+    SCC inner loop finishes."""
+
+    for node in nodes:
+        if node not in tracker.explored:
+            tracker.current_source = node
+            dfs_tracker(graph_dict, node, tracker)
 
 
 # Breadth-First Search
@@ -117,6 +160,54 @@ def prim( nodes, edges ):
                 if e[ 2 ] not in used:
                     heappush( usable_edges, e )
     return mst
+
+
+# Graph Reverse; for Question 2
+def graph_reverse(graph):
+    """Given a directed graph in forms of {tail:[head_list], ...}, compute
+    a reversed directed graph, in which every edge changes direction."""
+
+    reversed_graph = defaultdict(list)
+    for tail, head_list in graph.items():
+        for head in head_list:
+            reversed_graph[head].append(tail)
+    return reversed_graph
+
+
+# SCC; for Question 2
+def scc(graph):
+    """First runs dfs_loop on reversed graph with nodes in decreasing order,
+    then runs dfs_loop on original graph with nodes in decreasing finish
+    time order(obtained from first run). Return a dict of {leader: SCC}."""
+
+    out = defaultdict(list)
+    tracker1 = Tracker()
+    tracker2 = Tracker()
+    nodes = set()
+    reversed_graph = graph_reverse(graph)
+    for tail, head_list in graph.items():
+        nodes |= set(head_list)
+        nodes.add(tail)
+    nodes = sorted(list(nodes), reverse=True)
+    dfs_loop(reversed_graph, nodes, tracker1)
+    sorted_nodes = sorted(tracker1.finish_time,
+                          key=tracker1.finish_time.get, reverse=True)
+    dfs_loop(graph, sorted_nodes, tracker2)
+    for lead, vertex in groupby(sorted(tracker2.leader, key=tracker2.leader.get),
+                                key=tracker2.leader.get):
+        out[lead] = list(vertex)
+    return out
+
+
+# Linearizes a graph in its topological order; for Question 2
+def dfs_tpl_order(graph,start,path,n):
+    path = path + [start]
+    for edge in graph[start]: 
+        if edge not in path:
+            path = dfs_tpl_order(graph, edge,path,n)
+    print (n, start)
+    n -= 1
+    return path
 
 def main():
     #-----------------------Depth First/ Breadth First Search algorithms-------------------------#
@@ -222,7 +313,7 @@ def main():
     print('Visited: ', node_visited)
     print('Path :', path)
 
-#--------------------------Minimum Spanning Trees------------------------#
+    #--------------------------Minimum Spanning Trees------------------------#
 
     nodes = list("ABCDEFGHI")
     edges = [('A', 'B', 22), ('A','C', 9), ('A','D', 12), ('B', 'E', 34), ('B','C', 35), ('B','F', 36),
@@ -230,5 +321,74 @@ def main():
              ('F','H', 24), ('F','G',39), ('G','H', 25), ('G','I',21), ('H', 'I', 19)]
 
     print ('\n\nMinimum Spanning Tree:', prim(nodes,edges))    
+
+
+    #----------------------Question 2 - Hunter Henderson--------------------#
+    start = time.time()
+    '''    graph = defaultdict(list)
+    with open('SCC.txt') as file_in:
+    #with open('test.txt') as file_in:
+        for line in file_in:
+            x = line.strip().split()
+            x1, x2 = int(x[0]), int(x[1])
+            graph[x1].append(x2)'''
+            
+    dgraph2 = nx.Graph()
+    dgraph2.add_nodes_from(['1', '2','3','4','5','6','7','8','9','10','11','12'])
+    
+    dgraph2.add_edges_from([('1', '3')])
+    dgraph2.add_edges_from([('2', '1')])
+    dgraph2.add_edges_from([('3','2'), ('3', '5')])
+    dgraph2.add_edges_from([('4','4'),('4','2'), ('4', '12')])
+    dgraph2.add_edges_from([('5', '6'), ('5', '8')])
+    dgraph2.add_edges_from([('6', '7'), ('6', '8'), ('6', '10')])
+    dgraph2.add_edges_from([('7', '10')])
+    dgraph2.add_edges_from([('8', '9'), ('8', '10')])
+    dgraph2.add_edges_from([('9', '5'), ('9', '11')])
+    dgraph2.add_edges_from([('10', '9'), ('10', '11')])
+    dgraph2.add_edges_from([('11', '12')])
+    
+    
+    graph2 = {'1': set(['3']),
+             '2': set(['1']),
+             '3': set(['2', '5']),
+             '4': set(['1', '2', '12']),
+             '5': set(['6', '8']),
+             '6': set(['7', '8', '10']),
+             '7': set(['10']),
+             '8': set(['9', '10']),
+             '9': set(['5', '11']),
+             '10': set(['9', '11']),
+             '11': set(['12']),
+             '12': set()}
+    t1 = time.time() - start
+    print (t1)
+    groups = scc(graph2)
+    t2 = time.time() - start
+    print (round(t2,4))
+    top_5 = heapq.nlargest(5, groups, key=lambda x: len(groups[x]))
+    #sorted_groups = sorted(groups, key=lambda x: len(groups[x]), reverse=True)
+    result = []
+    for i in range(5):
+        try:
+            result.append(len(groups[top_5[i]]))
+            #result.append(len(groups[sorted_groups[i]]))
+        except:
+            result.append(0)
+            
+    print('\n\nQuestion 2A: Strongly connected components are:')
+    for key in groups:
+        print(groups[key])
+        
+    # print('\n\nQuestion 2B: Meta Graph Diagraph')
+    # drawGraph(dgraph2)
+    
+    n = len(graph2)
+    print('\n\nQuestion 2C: Topological order starting from \'1\'')
+    u = dfs_tpl_order(graph2, '1', [], n)
+    print(u)
+    
+#----------------------End of Program--------------------#
+
 if __name__ == "__main__":
     main()
